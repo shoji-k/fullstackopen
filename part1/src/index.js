ff962a74524b28c3a0dbe27741556121edfa1efb
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-01-05 20:07:53 +0900

    remove files and split components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+import Part from './components/Part'
+import Total from './components/Total'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <>
+      <Header course={course} />
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
